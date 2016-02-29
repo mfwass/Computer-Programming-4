b2c17ddb44c4536fc587745734dada80f4fb69f9
Michael Wass <michael@mfwass.com>
2016-02-29 08:57:10 -0500

    Fix formatting

--- Fibonacci/main.java
+++ Fibonacci/main.java
@@ -6,27 +6,27 @@ import java.io.InputStreamReader;
 public class main {
 
 	public static void main(String[] args) {
-        long fib = getFib();
-        long output = getFibNumber(fib);
-        System.out.println("Your answer is... " + output);
+            long fib = getFib();
+            long output = getFibNumber(fib);
+            System.out.println("Your answer is... " + output);
     }
 
 	private static long getFibNumber(long num) {
-		long previousFibo = 0;
-        long currentFibo = 1;
-        long nextFibo = 0;
-        if (num > 0) {
+            long previousFibo = 0;
+            long currentFibo = 1;
+            long nextFibo = 0;
+            if (num > 0) {
 	        for (int i = 0; i < num; i++) {
-		        nextFibo = previousFibo + currentFibo;
-                previousFibo = currentFibo;
-    		    currentFibo = nextFibo;
+                    nextFibo = previousFibo + currentFibo;
+                    previousFibo = currentFibo;
+                    currentFibo = nextFibo;
             }
         }
-    return previousFibo;
+        return previousFibo;
 	}
 
 	private static int getFib() {
-		InputStreamReader stdin = new InputStreamReader(System.in);
+        InputStreamReader stdin = new InputStreamReader(System.in);
         BufferedReader console = new BufferedReader(stdin);
         String  msg = "";
         int fib = 0;
@@ -35,12 +35,12 @@ public class main {
             msg = console.readLine();
             fib = Integer.parseInt(msg);
         } catch (IOException e) {
-        	System.out.println("Error with input!");
+            System.out.println("Error with input!");
         }
         if (fib < 0) {	
-        	System.out.println("Error: You must enter a number greater than or equal to 0.");
+            System.out.println("Error: You must enter a number greater than or equal to 0.");
             System.exit(1);
         }
         return fib;
-	}
+        
 }
