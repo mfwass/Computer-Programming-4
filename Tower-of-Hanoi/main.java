ab3201f75116fe18d907e3d8da3fdbaf7c27e7dc
Michael Wass <michael@mfwass.com>
2016-03-08 11:04:12 -0500

    Tower of Hanoi
    TODO: Graphical

---
+++ Tower-of-Hanoi/main.java
@@ -0,0 +1,51 @@
+import java.io.*;
+
+public class main {
+
+    public static void main(String[] args) {
+        int input = getInput();
+        int numOfMoves = getNumOfMoves(input);
+        System.out.println("It will take you " + numOfMoves + " moves to complete this.");
+        hanoi(input,'A','B','C');
+    }
+
+    private static int getNumOfMoves(int input) {
+        int numOfMoves = (int) (Math.pow(2, input) - 1);
+        // FUN DEBUG PRINTS! :D
+        // System.out.println("input = " + input + "; numOfMoves = " + (int) (Math.pow(2, input) - 1));
+        return numOfMoves;
+    }
+
+    private static int getInput() {
+        InputStreamReader stdin = new InputStreamReader(System.in);
+        BufferedReader console = new BufferedReader(stdin);
+        String  msg = "";
+        int input = 0;
+        try {
+            System.out.print("What is the number of disks? ");
+            msg = console.readLine();
+            input = Integer.parseInt(msg);
+        } catch (IOException e) {
+            System.out.println("Error with input!");
+        } catch (NumberFormatException e) {
+            System.out.println("Error: Bad input! You must enter in a NUMBER greater than 0 and less than 64!");
+            System.exit(1);
+        }
+        if (input <= 0 || input >= 64) {    
+            System.out.println("Error: You must enter a number greater than 0 and less than 64.");
+            System.exit(1);
+        }
+        return input;
+    }
+    
+    public static void hanoi(int i, char a, char b, char c) {
+        if (i<=1) {
+            System.out.println("Move disk from " + a + " to " + c + ".");
+        } else {
+            hanoi(i-1,a,c,b);
+            System.out.println("Move disk from " + a + " to " + c + ".");
+            hanoi(i-1,b,a,c);
+        }
+    }
+
+}
