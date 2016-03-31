b51f3be9254148a621e36d55ede4ec2e276036ed
Michael Wass <mwass@students.westernwayne.org>
2016-03-31 08:32:38 -0400

    cleanup: whitespace ahhh

--- Tic-Tac-Toe/main.java
+++ Tic-Tac-Toe/main.java
@@ -11,14 +11,14 @@ import javax.swing.JTextPane;
 
 @SuppressWarnings("serial")
 public class main extends JFrame {
-    
+
     //Create stuff
     public static boolean move = false;
     public static JTextPane gameBoard = new JTextPane();
     public static JTextField txtInput = new JTextField("");
     static JLabel lbl = new JLabel("Player " + (char)(88+(move? 1 : 0)*-9) + ", make your move.");
-    public static char board[][] = new char[3][3]; 
-    
+    public static char board[][] = new char[3][3];
+
     public main() {
         //Create window
         setTitle("Tic-Tac-Toe");
@@ -40,22 +40,22 @@ public class main extends JFrame {
         c.gridwidth = 3;
         c.gridy = 0;
         add( gameBoard ,c);
-        
+
         //Label
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridy = 1;
         c.ipady = 0;
         add( lbl , c);
-        
+
         //Input Box
-        txtInput.addActionListener(enterPress); 
+        txtInput.addActionListener(enterPress);
         c.ipady=10;
         c.weightx = 1;
         c.weighty = 1;
         c.gridy = 2;
         add( txtInput ,c);
     }
-    
+
     @SuppressWarnings("unused")
     public static void main(String[] args) {
         //Initialize game
@@ -72,34 +72,34 @@ public class main extends JFrame {
 
     public static void drawBoard() {
         //Draw Board
-        gameBoard.setText("Board      Key\n" + 
-                          board[0][0] + "█" + board[1][0] + "█" +board[2][0] + "     1 2 3\n" + 
-                          "█████\n" + 
-                          board[0][1] + "█" + board[1][1] + "█" +board[2][1] + "     4 5 6\n" + 
-                          "█████\n" + 
+        gameBoard.setText("Board      Key\n" +
+                          board[0][0] + "█" + board[1][0] + "█" +board[2][0] + "     1 2 3\n" +
+                          "█████\n" +
+                          board[0][1] + "█" + board[1][1] + "█" +board[2][1] + "     4 5 6\n" +
+                          "█████\n" +
                           board[0][2] + "█" + board[1][2] + "█" +board[2][2] + "     7 8 9"
                             );
     }
     public static void getInput(int input) {
-        
+
         //If eligible number
         if (input < 1 || input > 9) {
             txtInput.setText("ERROR: Not an eligible number.");
         }
         else {
             txtInput.setText("");
-            
+
         //Convert Input to Board Coordinates
         int xx = ((input-1)%3);
         int yy = ((input-1)/3);
-            
+
         //Check if space is empty
         if (board[xx][yy] == ' ') {
-            
+
             //Put an X or O
             board[xx][yy] = (char)(88+(move? 1 : 0)*-9);
             drawBoard();
-                
+
             //Check for win
             if (checkWin()) {
                 lbl.setText("Player " + (char)(88+(move? 1 : 0)*-9) + " has won!!!!!!");
@@ -110,25 +110,25 @@ public class main extends JFrame {
                     //Draw
                     lbl.setText("The game is a draw.");
                 }
-                else {    
+                else {
                     //Next move
                     move = !move;
                     lbl.setText("Player " + (char)(88+(move? 1 : 0)*-9) + ", make your move.");
                 }
             }
         }
-            
+
         else {
-            
+
             txtInput.setText("Error: Space " + input + " is already taken.");
         }
     }
     }
-    
+
     //Win Checker
     public static boolean checkWin() {
         boolean win = false;
-        
+
         //Columns
         for (int xx = 0; xx < 3; xx++) {
             if (board[xx][0] == board[xx][1] &&  board[xx][0] == board[xx][2] && board[xx][0] != ' ') {
@@ -141,7 +141,7 @@ public class main extends JFrame {
                 win = true;
             }
         }
-        
+
         //Diagonals
         if (board[1][1] == board[0][0] && board[1][1] == board[2][2] && board[1][1] != ' ') {
             win = true;
@@ -149,11 +149,11 @@ public class main extends JFrame {
         if (board[1][1] == board[0][2] && board[1][1] == board[0][2] && board[1][1] != ' ') {
             win = true;
         }
-        
-        
+
+
         return win;
     }
-    
+
     //Check how much of board is filled
     public static int checkMoves() {
         int moves = 0;
@@ -168,14 +168,14 @@ public class main extends JFrame {
     }
 
     Action enterPress = new AbstractAction() {
-    @Override    
+    @Override
         public void actionPerformed(ActionEvent e) {
             //Get Number Input
             int i = 0;
             try {  i = Integer.parseInt(txtInput.getText().replace(" ", ""));}
             catch (NumberFormatException n) {
                 txtInput.setText("ERROR: You have not inputted a number.");
-            }        
+            }
             getInput(i);
         }
     };
